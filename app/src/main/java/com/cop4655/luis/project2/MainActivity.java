bc7fabce135ad30a881f70681c413b0ebe11e75e
Luis Santos <santos.luis@titans.easternflorida.edu>
2017-06-17 18:25:59 -0400

    Project Created

---
+++ app/src/main/java/com/cop4655/luis/project2/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+ * Luis Santos
+ * COP 4655
+ * Project 2
+ * Tic-Tac-Toe
+ */
+//gittest
+package com.cop4655.luis.project2;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
